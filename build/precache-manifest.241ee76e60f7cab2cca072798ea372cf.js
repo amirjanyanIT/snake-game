0720006d8f3df3f82e6108d096b62e7df9937c58
Hovhannes Amirjanyan <amirjanyanit@gmail.com>
2023-11-24 00:19:25 +0400

    build

---
+++ build/precache-manifest.241ee76e60f7cab2cca072798ea372cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457fa561bf6a60a1b0234a8a2e360816",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1643160fc75e599bc0a",
+    "url": "./static/css/main.cb3e39b4.chunk.css"
+  },
+  {
+    "revision": "46a14ca58fc9f4c51e7a",
+    "url": "./static/js/2.a999d7d4.chunk.js"
+  },
+  {
+    "revision": "f1643160fc75e599bc0a",
+    "url": "./static/js/main.fb8da268.chunk.js"
+  },
+  {
+    "revision": "189140b93d641027faf1",
+    "url": "./static/js/runtime~main.dffaff4d.js"
+  },
+  {
+    "revision": "964111990ca3937960147b80e8d07e61",
+    "url": "./static/media/ARCADECLASSIC.96411199.TTF"
+  }
+]);
